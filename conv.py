4499ffcdadb9fe7e4137128ef36359dcd63e3267
Dmytro <dimakush1@gmail.com>
2019-10-08 00:51:47 +0300

    update conv.py in order to read model from the json file

--- conv.py
+++ conv.py
@@ -1,7 +1,17 @@
 import coremltools
+from keras.models import model_from_json
+import json
+
+modelName = 'trained_3'
+
+with open('%s.json' % modelName, 'r') as f:
+    model_json  = f.read()
+model = model_from_json(model_json)
+print('model -> ', model)
+model.load_weights('%s.h5' % modelName)
 
 coreml_model = coremltools.converters.keras.convert(
-    'yolo-tiny.h5',
+    model,
     input_names='image',
     image_input_names='image',
     input_name_shape_dict={'image': [None, 416, 416, 3]},
