5c9783f678fc469bb27ed2c3210df9908e64e740
Olha Horin <horinolha@gmail.com>
2019-11-16 15:54:53 +0200

    added Drone scene

---
+++ yolov3/yolov3/ViewControllers/DroneViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DroneViewController.swift
+//  yolov3
+//
+//  Created by DmytOlh on 16/11/2019.
+//  Copyright © 2019 DmytOlh. All rights reserved.
+//
+
+import UIKit
+
+class DroneViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+  }
+  
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+}
