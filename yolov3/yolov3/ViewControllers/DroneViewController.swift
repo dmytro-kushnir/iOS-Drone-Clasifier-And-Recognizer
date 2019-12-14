019a92398e1db4e2c693beb6308574a09c11f0ea
Dmytro Kushnir <dimakush1@gmail.com>
2019-12-14 12:38:01 +0200

    perform object recognition from drone stream

--- yolov3/yolov3/ViewControllers/DroneViewController.swift
+++ yolov3/yolov3/ViewControllers/DroneViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 import SystemConfiguration.CaptiveNetwork
 import CoreLocation
 import VideoToolbox
-import AVFoundation
 
 class DroneViewController: UIViewController, CLLocationManagerDelegate, VideoFrameDecoderDelegate {
   @IBOutlet weak var videoView: UIImageView!
@@ -19,13 +18,10 @@ class DroneViewController: UIViewController, CLLocationManagerDelegate, VideoFra
   let tello = Tello()
   var frameDecoder: VideoFrameDecoder!
   var isConnected = false
-  let captureSession = AVCaptureSession()
-  let videoOutput = AVCaptureVideoDataOutput()
-  var previewLayer: AVCaptureVideoPreviewLayer?
   weak var modelProvider: ModelProvider!
   var predictionLayer: PredictionLayer!
-  let smoother = Smoother()
-  let semaphore = DispatchSemaphore(value: 1)
+  var processed = false
+  var processStarted = false
   
   var lastTimestamp = CMTime()
   let maxFPS = 30
@@ -105,6 +101,11 @@ class DroneViewController: UIViewController, CLLocationManagerDelegate, VideoFra
     } else {
         printWifiStatus()
     }
+    
+    modelProvider = ModelProvider.shared
+    modelProvider.delegate = self
+    predictionLayer = PredictionLayer()
+    predictionLayer.addToParentLayer(videoView.layer)
   }
   
   func printWifiStatus() {
@@ -126,6 +127,19 @@ class DroneViewController: UIViewController, CLLocationManagerDelegate, VideoFra
     super.viewDidAppear(animated)
     
     droneControlMethod(command: "startstream")
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    modelProvider.delegate = self
+    predictionLayer.clear()
+    processed = false
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    predictionLayer.hide()
+    predictionLayer.clear()
   }
   
   // Check if device is connected to Tello WiFi
@@ -165,14 +179,17 @@ class DroneViewController: UIViewController, CLLocationManagerDelegate, VideoFra
       if let cgImage = cgImage {
           DispatchQueue.main.async {
               self.videoView.image = UIImage(cgImage: cgImage)
-              self.modelProvider = ModelProvider.shared
-              self.predictionLayer = PredictionLayer()
-              self.predictionLayer.update(imageViewFrame: self.videoView.frame,
-                                   imageSize: CGSize(width: 720, height: 1280))
-              if let previewLayer = self.previewLayer {
-                self.videoView.layer.addSublayer(previewLayer)
-                self.predictionLayer.addToParentLayer(self.videoView.layer)
-                self.resizePreviewLayer()
+            if !self.processed {
+                guard let image = self.videoView.image else {
+                  self.showAlert(title: "Warning!", msg: "Image from drone can't be obtained")
+                  return
+                }
+                self.processStarted = true
+                self.modelProvider.predict(frame: image)
+              } else {
+                self.processed = false
+                self.predictionLayer.hide()
+                self.predictionLayer.clear()
               }
           }
       } else {
@@ -220,34 +237,36 @@ extension DroneViewController: ModelProviderDelegate {
       }
       return
     }
-    predictionLayer.clear()
-    if Settings.shared.isSmoothed {
-      smoother.addToFrameHistory(predictions: predictions)
-      predictionLayer.addBoundingBoxes(predictions: smoother.getSmoothedBBoxes())
-    } else {
+    if processStarted {
       predictionLayer.addBoundingBoxes(predictions: predictions)
+      predictionLayer.show()
+      processed = true
+      processStarted = false
     }
-    predictionLayer.show()
-//    self.fpsLabel.text = "FPS: " + String(format: "%.2f", stat.fps)
-//    self.secPerFrameLabel.text = "SecPerFrame: " + String(format: "%.2f", stat.timeForFrame)
   }
   
 }
 
-extension DroneViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+// MARK: - UIImagePickerControllerDelegate
+
+extension DroneViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
-  public func captureOutput(_ output: AVCaptureOutput,
-                            didOutput sampleBuffer: CMSampleBuffer,
-                            from connection: AVCaptureConnection) {
-    let timestamp = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
-    let deltaTime = timestamp - lastTimestamp
-    if deltaTime >= CMTimeMake(value: 1, timescale: Int32(maxFPS)) {
-      if let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
-        if let frame = UIImage(pixelBuffer: imageBuffer) {
-          modelProvider.predict(frame: frame)
-        }
-      }
+  @objc func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    self.dismiss(animated: true)
+    predictionLayer.show()
+  }
+  
+  @objc func imagePickerController(_ picker: UIImagePickerController,
+                             didFinishPickingMediaWithInfo info:
+    [UIImagePickerController.InfoKey : Any]) {
+    if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+      self.videoView.image = pickedImage
+      self.videoView.backgroundColor = .clear
+      predictionLayer.update(imageViewFrame: videoView.frame, imageSize: pickedImage.size)
+      predictionLayer.clear()
+      processed = false
     }
+    self.dismiss(animated: true)
   }
 
 }
